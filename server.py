3643df1e96564b899f4a85495fb2fa0dff019ae8
gaylelemuel.bit <gaylelemuel@gmail.com>
2026-01-21 22:22:44 -0600

    Assignment1

---
+++ server.py
@@ -0,0 +1,78 @@
+from flask import Flask  
+
+app = Flask(__name__) #instance of Flask
+
+# http://127.0.0.1:5000/
+@app.route("/",methods=["GET"])
+def index():
+  return "welcome to Framework"
+
+# http://127.0.0.1:5000/hello
+@app.route("/hello", methods=['GET'])
+def hello():
+  return "Hello World from flask"
+
+
+# http://127.0.0.1:5000/cohort-63  
+@app.route("/cohort-63", methods=["GET"])
+def cohort63():
+     student_list = ["robert", "luis", "barney", "lemuel", "john", "reece", "angel"]
+     return student_list
+
+
+# http://127.0.0.1:5000/cohort-99  
+@app.route("/cohort-99", methods=["GET"])
+def cohort99():
+     student_list = ["pam", "michael", "angela", "dwight"]
+     return student_list
+
+
+@app.route("/contact", methods=["GET"])
+def contact():
+  info = {
+    "email": "bigdog@gmail.com",
+    "phone": "561-682-5690"
+  }
+  return info
+
+
+@app.route("/course-info", methods=["GET"])
+def course_info():
+  course_data = {
+    "Title": " Python with Flask",
+    "Duration": "4 Sessions",
+    "Level": "Beginner"
+  }
+  return course_data
+
+
+
+#------Copouns----------
+#------Aissignment 1---------
+@app.route("/coupons", methods=["GET"])
+def coupons(): 
+  coupons_list= [
+    {"_id": 1, "code": "WELCOME10", "discount": 10},
+    {"_id": 2, "code": "SPOOKY25", "discount": 25},
+    {"_id": 3, "code": "VIP50", "discount": 50}
+  ]
+
+  return coupons_list
+
+  @app.route("/coupons/count", methods=["GET"])
+  def coupons_count(): 
+      count = len(coupons_list)
+
+      return {"count": count}
+
+  
+
+
+
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
+# When this file is run directly: __name__== "__main__"
+# When this file is imported as a module: _name_== "server.py"
+    
